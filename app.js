6c587a8bd419567c2cdf43b9b6ab890b07f8dd25
Kostas Mamalis <kostas@masterthought.net>
2020-03-24 18:27:59 +0000

    feature(json-server): use json-server instead of express directly

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express');
+const jsonServer = require('json-server');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+/*
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+*/
+
 
 const getApp = (configuration) => {
-    const app = express();
+    const app = jsonServer.create();
     const RoutingService = require('./dist/routes/index').default;
     const indexRouter = new RoutingService(configuration).routes;
+    const router = jsonServer.router('db.json')
+
     app.use(logger('dev'));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
     app.use('/', indexRouter);
+    app.use(router);
     return app;
 }
 
